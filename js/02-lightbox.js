233e770ce2476bcccbaaefd059f47ba63bbeb5e5
ArthurKorchak <korchak.arthur@gmail.com>
2022-08-05 13:31:19 +0300

    upd

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,17 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const trgForAdd = document.querySelector('.gallery');
+
+galleryGen(galleryItems);
+new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250, });
+
+function galleryGen(galleryItems) {
+    const dataSet = galleryItems.reduce((acc, { preview, original, description }) => {
+        return acc +=
+            `<a class="gallery__item" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>`
+    }, '');
+    trgForAdd.innerHTML = dataSet;
+};
